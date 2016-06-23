6c2dfdbc6167d4d1467fb54cdad97ffee1e95876
Gautam krishna.R <r.gautamkrishna@gmail.com>
2016-06-23 21:44:06 +0530

    version bump

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
-# -*- coding: utf-8 -*-
-from setuptools import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
 from sys import exit,version
-import os
 import sys
 if version < '3.0.0':
     print("Python 2 is not supported...")
     sys.exit(1)
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__),fname),encoding="utf8").read()
-
 
 setup(
     name='socli',
@@ -18,12 +15,12 @@ setup(
     entry_points = {"console_scripts": ['socli = socli.socli:main']},
     install_requires=['BeautifulSoup4','requests'],
     requires=['BeautifulSoup4','requests'],
-    version='1.3',
+    version='1.4',
     url='http://www.github.com/gautamkrishnar/socli',
     keywords="stack overflow cli",
     license='BSD',
     author='Gautam krishna R',
     author_email='r.gautamkrishna@gmail.com',
     description='Stack overflow commnand line interface. SoCLI allows you to search and browse stack overfow from the terninal.',
-    long_description=read('pypi-readme.txt')
+    long_description="Go to www.github.com/gautamkrishnar/socli"
     )
