1ef039ea1abc8b09bac46d2f3ee567759580931f
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-11-15 03:46:20 +0530

    Added login_required decorator

--- socli/auth.py
+++ socli/auth.py
@@ -1,6 +1,8 @@
 import os
 
+from functools import wraps
 from getpass import getpass
+
 from requests import Session
 
 # Supporting input in Python 2/3
@@ -76,4 +78,30 @@ def login(email, password):
         resp['error'] = 'Stackoverflow is probably down. Please try again.'
 
     return resp
+
+
+def login_required(func):
+    """
+    :desc: decorator method to check user's login status
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        is_login = False
+        if os.path.exists(COOKIES_FILE_PATH):
+            cookiejar = LWPCookieJar(filename=COOKIES_FILE_PATH)
+            cookiejar.load()
+
+            if len(cookiejar):
+                is_login = True
+            else:
+                os.remove(COOKIES_FILE_PATH)
+
+        if not is_login:
+            print('You are not logged in.')
+            return None
+        else:
+            return func(*args, **kwargs)
+
+    return wrapper
 
