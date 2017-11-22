49148ccdc919874c5a043f4d402b65d29f2fd7d5
Aljosha Balitzki <albalitz@users.noreply.github.com>
2017-11-22 10:05:53 +0100

    Add 'q' to close socli in interactive mode (#101)
    This adds 'q' to the footer actions of the interactive mode,
    allowing to exit socli from the answers screen without going back to the
    list first.

--- socli/socli.py
+++ socli/socli.py
@@ -197,7 +197,7 @@ class QuestionPage(urwid.WidgetWrap):
             body=self.answer_text,
             footer= urwid.Pile([
                 QuestionURL(question_url),
-                UnicodeText((u"\u2191: previous answer, \u2193: next answer, o: open in browser, \u2190: back").encode('utf-8'))
+                UnicodeText(u'\u2191: previous answer, \u2193: next answer, o: open in browser, \u2190: back, q: quit').encode('utf-8'))
             ])
         )
         return answer_frame
@@ -225,6 +225,8 @@ class QuestionPage(urwid.WidgetWrap):
                 self._invalidate()
                 answer_frame = self.makeFrame(self.data)
                 urwid.WidgetWrap.__init__(self, answer_frame)
+        elif key in {'q', 'Q'}:
+            sys.exit(0)
 
 
 class AnswerText(urwid.WidgetWrap):
