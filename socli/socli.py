74b99a6d3086d3fa76fcd31f4090e5382bcff098
insiyaa <insiyahhajoori@gmail.com>
2018-03-21 20:49:40 +0530

    Added new feature to view documentation of python

--- socli/socli.py
+++ socli/socli.py
@@ -1151,6 +1151,93 @@ def auth_callback(response):
     else:
         print_fail("\n " + response['message']  + " \n")
 
+def doc_support():
+    from urllib.request import urlopen
+
+    def parseHTML(url):
+        client = urlopen(url)
+        page_html = client.read()
+        client.close()
+        page_soup = soup(page_html,"html.parser")
+        return page_soup
+
+    def getNavList(page_soup):
+        index = page_soup.find_all("div", {"class":"toctree-wrapper"})
+        index_items = index[0].find_all("a", {"reference internal"})
+        return index_items
+
+    def printNavList(index_items):
+        for item in index_items:
+            print(item.text)
+
+    def printDocData(nav, nav_url, index_items):
+        nav = nav.rstrip('.')
+        k = len(nav)
+        for item in index_items:
+            if nav in item.text[:k]:
+                div_id = item['href'].split("#",1)
+                if len(div_id) == 2:
+                    div_id = div_id[1]
+                else:
+                    div_id = None
+                page_soup = parseHTML(nav_url + item['href'])
+                req_div = page_soup.find_all("div",{"id":div_id})
+                print(req_div[0].text)
+                return True
+        print("Invalid choice!")
+        return False
+
+
+    while(1):
+        print("1. Python 3.6\n2. Python 2.7")
+        print("Enter your choice:")
+        try:
+            version = int(input())
+        except:
+            print("Invalid choice: Enter 1 or 2")
+            continue
+        
+        if version not in [1,2]:
+            print("Invalid choice, try again.")
+            continue
+        else:
+            if version == 1:
+                placeholder = 3
+            else:
+                placeholder = 2.7
+            while 1:
+                nav_url = "https://docs.python.org/{}/reference/".format(placeholder)
+                my_url = "https://docs.python.org/{}/reference/index.html".format(placeholder)
+                page_soup = parseHTML(my_url)
+                index_items = getNavList(page_soup)
+
+                printNavList(index_items)
+                print("Enter:")
+                nav = input()
+                if nav == "q":
+                    flag = 0
+                    break
+                elif nav == "c":
+                    flag = 1
+                    break
+                if not printDocData(nav, nav_url, index_items):
+                    continue
+                print("q: quit\nl: documentation menu\nc: change python version")
+                choice = input()
+                if choice == "q":
+                    flag = 0
+                    break
+                elif choice == "l":
+                    continue
+                elif choice == "c":
+                    flag = 1
+                    break
+                else:
+                    print("Invalid choice.")
+        if flag == 0:
+            flag = 1
+            break
+
 def parseArguments(command):
     """
     Parses the command into arguments and flags
@@ -1200,6 +1287,7 @@ def parseArguments(command):
                                                   " \"foo bar\"'s most voted answer")
     parser.add_argument('--login', '-l', action='store_true', help="Prompt a user for email and password to login to StackOverflow.")
     parser.add_argument('--logout', action='store_true', help="Log a user out of StackOverflow")
+    parser.add_argument('--doc', action='store_true', help="View Python documentation")
 
     namespace = parser.parse_args(command)
     return namespace
@@ -1250,6 +1338,8 @@ def main():
         google_search = False
         tag = namespace.tag
         hastags()
+    if namespace.doc: # If --doc flag is present
+        doc_support()
     if namespace.res != None: #If --res flag is present
         questionNumber = namespace.res
         if namespace.query != [] or namespace.tag != None: #There must either be a tag or a query
