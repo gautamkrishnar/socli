4a8d5e6bce04bace92ac25db6aa626c26e052724
gautamkrishnar <r.gautamkrishna@gmail.com>
2020-07-08 23:37:04 +0530

    brew formula release

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     requires=['BeautifulSoup4','requests','colorama','PyStackExchange', 'urwid'],
-    version='4.4',
+    version='4.5',
     url='http://www.github.com/gautamkrishnar/socli',
     keywords="stack overflow cli",
     license='BSD',
