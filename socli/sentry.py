332bbd84477498a045cfdd7b56b21127fa366a2b
gautamkrishnar <r.gautamkrishna@gmail.com>
2020-10-04 14:55:38 +0530

    Set sample rate to 1.0

--- socli/sentry.py
+++ socli/sentry.py
@@ -3,7 +3,7 @@ import sentry_sdk
 
 sentry_sdk.init(
     "https://95c4106659044cbda2ea0fe499f4be7e@o323465.ingest.sentry.io/5445901",
-    traces_sample_rate=0.5
+    traces_sample_rate=1.0
 )
 
 from socli.socli import main
