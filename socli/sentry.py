4fbe6c21f35a535618801301e1dc367c135bf119
gautamkrishnar <r.gautamkrishna@gmail.com>
2020-10-04 16:09:13 +0530

    Added version to sentry.py

--- socli/sentry.py
+++ socli/sentry.py
@@ -1,9 +1,11 @@
 # Initialize Sentry
 import sentry_sdk
+from socli.version import __version__
 
 sentry_sdk.init(
     "https://95c4106659044cbda2ea0fe499f4be7e@o323465.ingest.sentry.io/5445901",
-    traces_sample_rate=1.0
+    traces_sample_rate=1.0,
+    release="socli-" + __version__
 )
 
 from socli.socli import main
